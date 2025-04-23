1ee309b1aacf96a2487e5e13b43eb0efa11f0959
Daniel Alvariño <danialvarino@gmail.com>
2025-04-23 23:25:52 +0200

    more

--- filedatechange.py
+++ filedatechange.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import datetime
+import json
 import re
 from PIL import Image
 from PIL.ExifTags import TAGS
@@ -47,41 +48,91 @@ def get_date_from_filename(full_path: str, filename: str) -> str:
     Returns:
         str: The extracted date and time as a string.
     """
-    # try to extract the date from EXIF data using exifread
-    json_data = get_exif(full_path)
-    print (f"EXIF data: {json_data}")
+    # if the file is a jpg file, try to extract the date from the EXIF data
+    if filename.endswith('.jpg') or filename.endswith('.jpeg'):
+        print (f"File '{filename}' is a jpg file.")
+        # check if the file exists
+        if not os.path.isfile(full_path):
+            print(f"File '{full_path}' does not exist.")
+            return None
+        # check if the file is empty
+        if os.path.getsize(full_path) == 0:
+            print(f"File '{full_path}' is empty.")
+            return None
+        # check if the file is a valid image
+        # try:
+        #     img = Image.open(full_path)
+        #     img.verify()
+        # except Exception as e:
+        #     print(f"File '{full_path}' is not a valid image. Error: {e}")
+        #     return None
+        # try to extract the date from EXIF data using exifreader
+        json_data = get_exif(full_path)
+        print (f"EXIF data: {json_data}")
 
-    # data = exif.parse(full_path)
-    # print (f"EXIF data: {data}")
+        # data = exif.parse(full_path)
+        # print (f"EXIF data: {data}")
 
-    if 'DateTimeOriginal' in json_data:
-        # Convert the date string to a datetime object
-        dt = datetime.datetime.strptime(json_data['DateTimeOriginal'], "%Y:%m:%d %H:%M:%S")
-        # Convert the datetime object to a string in the desired format
-        dt = dt.strftime("%Y-%m-%d %H:%M:%S")
-        # Return the formatted date string
-        return dt
-    elif 'DateTimeDigitalized' in json_data:
-        # Convert the date string to a datetime object
-        dt = datetime.datetime.strptime(json_data['DateTimeDigitalized'], "%Y:%m:%d %H:%M:%S")
-        # Convert the datetime object to a string in the desired format
-        dt = dt.strftime("%Y-%m-%d %H:%M:%S")
-        # Return the formatted date string
-        return dt
-    elif 'DateTime' in json_data:
-        # Convert the date string to a datetime object
-        dt = datetime.datetime.strptime(json_data['DateTime'], "%Y:%m:%d %H:%M:%S")
-        # Convert the datetime object to a string in the desired format
-        dt = dt.strftime("%Y-%m-%d %H:%M:%S")
-        # Return the formatted date string
-        return dt
-    else:
-        print(f"File '{filename}' has no valid date and time EXIF data.")
+        if 'DateTimeOriginal' in json_data:
+            # Convert the date string to a datetime object
+            dt = datetime.datetime.strptime(json_data['DateTimeOriginal'], "%Y:%m:%d %H:%M:%S")
+            # Convert the datetime object to a string in the desired format
+            dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+            # Return the formatted date string
+            return dt
+        elif 'DateTimeDigitalized' in json_data:
+            # Convert the date string to a datetime object
+            dt = datetime.datetime.strptime(json_data['DateTimeDigitalized'], "%Y:%m:%d %H:%M:%S")
+            # Convert the datetime object to a string in the desired format
+            dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+            # Return the formatted date string
+            return dt
+        elif 'DateTime' in json_data:
+            # Convert the date string to a datetime object
+            dt = datetime.datetime.strptime(json_data['DateTime'], "%Y:%m:%d %H:%M:%S")
+            # Convert the datetime object to a string in the desired format
+            dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+            # Return the formatted date string
+            return dt
+        else:
+            print(f"File '{filename}' has no valid date and time EXIF data.")
+
+    # if fullpath.json exists, read the date from it
+    if os.path.isfile(full_path + '.json'):
+        print (f"File '{full_path}.json' exists.")
+        with open(full_path + '.json', 'r') as f:
+            # get json data from the file
+            data = f.read()
+            # get photoTakenTime field from json data
+            json_data = json.loads(data)
+            # check if the field exists
+            if 'photoTakenTime' in json_data:
+                print (f"File '{full_path}.json' has 'photoTakenTime' field.")
+                # get the date and time from the field
+                dt = json_data['photoTakenTime']
+                # convert the date and time to a datetime object
+                dt = datetime.datetime.fromtimestamp(int(dt['timestamp']))
+                # convert the datetime object to a string in the desired format
+                dt = dt.strftime("%Y-%m-%d %H:%M:%S")
+                # return the formatted date string
+                return dt
+            else:
+                print(f"File '{full_path}.json' has no valid date and time EXIF data.")
 
     # if the file name matchs the regex pattern 'IMG-YYYYMMM-WA[0-9]*.jpg'
     if re.match(r'IMG-\d{8}-WA\d+\.jpg', filename):
         # extract the date from the file name
         print(f"File '{filename}' matchs the regex pattern 'IMG-YYYYMMM-WA[0-9]*.jpg'")
+        year = filename.split('-')[1][:4]
+        month = filename.split('-')[1][4:6]
+        day = filename.split('-')[1][6:]
+        date_str = f'{year}-{month}-{day} 00:00:00'
+        return date_str
+    
+    # if the file name matchs the regex pattern 'IMG-YYYYMMM-WA[0-9]*\.*.jpg'
+    if re.match(r'IMG-\d{8}-WA\d+\.*.*\.jpg', filename):
+        # extract the date from the file name
+        print(f"File '{filename}' matchs the regex pattern 'IMG-YYYYMMM-WA[0-9]*\.*.jpg'")
         year = filename.split('-')[1][:4]
         month = filename.split('-')[1][4:6]
         day = filename.split('-')[1][6:]
@@ -113,7 +164,31 @@ def get_date_from_filename(full_path: str, filename: str) -> str:
         second = filename.split('_')[1][4:6]
         date_str = f'{year}-{month}-{day} {hour}:{minute}:{second}'
         return date_str
+    
+    # if the file name matchs the regex pattern 'VID-YYYYMMDD-\.*.*'
+    if re.match(r'VID-\d{8}-\.*.*', filename):
+        # extract the date from the file name
+        print(f"File '{filename}' matchs the regex pattern 'VID-YYYYMMDD-\.*.*'")
+        year = filename.split('-')[1][:4]
+        month = filename.split('-')[1][4:6]
+        day = filename.split('-')[1][6:]
+        date_str = f'{year}-{month}-{day} 00:00:00'
+        return date_str
+    
+    # if the file name matchs the regex pattern 'Screenshot_YYYYMMDD-HHMMSS\.*.*'
+    if re.match(r'Screenshot_\d{8}-\d{6}.*', filename):
+        # extract the date from the file name
+        print(f"File '{filename}' matchs the regex pattern 'Screenshot_YYYYMMDD-HHMMSS\.*.*'")
+        year = filename.split('_')[1][:4]
+        month = filename.split('_')[1][4:6]
+        day = filename.split('_')[1][6:8]
+        hour = filename.split('-')[1][:2]
+        minute = filename.split('-')[1][2:4]
+        second = filename.split('-')[1][4:6]
+        date_str = f'{year}-{month}-{day} {hour}:{minute}:{second}'
+        return date_str
 
+    print(f"File '{filename}' does not match any known date format.")
     return None
 
 
@@ -151,9 +226,17 @@ if __name__ == "__main__":
         # Get all files in the directory
         print(f"'{file_path}' is a directory.")
         files = get_files_in_directory(file_path)
-        print(f"Files in directory '{file_path}': {files}")
+        #print(f"Files in directory '{file_path}': {files}")
         # Set the modification date for each file
         for file in files:
+            if file.endswith('.json'):
+                print(f"Skipping json file: {file}")
+                continue
+            # check if the file is a directory
+
+            if os.path.isdir(os.path.join(file_path, file)):
+                print(f"Skipping directory: {file}")
+                continue
             print(f"Setting date for file: {file}")
             full_path = os.path.join(file_path, file)
             date_str = get_date_from_filename(full_path,file)
@@ -170,7 +253,10 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Get the file date from the filename
-    date_str = get_date_from_filename(os.path.basename(file_path))
+    date_str = get_date_from_filename(file_path,os.path.basename(file_path))
+    if date_str is None:
+        print(f"Could not extract date from file '{file_path}'.")
+        sys.exit(1)
     print (f"Extracted date string: {date_str}")
 
     # Set the file's modification date
